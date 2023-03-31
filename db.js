5fbc8d580935277f99097f743213b27f00ef586f
proger-man <121639652+proger-man@users.noreply.github.com>
2023-03-31 22:43:22 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'postgres',
-    password: 'jR428%fgLK9!',
+    user: 'proger',
+    password: 'root#E1',
     host: 'localhost',
     port: 5432,
     database: 'nkse_api',
