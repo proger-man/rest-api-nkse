7de3bac68fc7a589b128732358febde1dae4b2cb
proger-man <121639652+proger-man@users.noreply.github.com>
2023-04-06 22:24:21 +0300

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -69,7 +69,7 @@ class UserController {
     const isRowExistsLogin = checkLogin.rows[0];
     const isRowExistsPassword = checkPassword.rows[0];
     if (isRowExistsLogin.exists && isRowExistsPassword.exists) {
-      res.json(checkPosition.rows.post);
+      res.json(checkPosition.rows);
     } else {
       res.json("Error");
     }
